fa27bc38d2d8721aba19ed8794e5510661f8f541
ws.choi <ws.choi@meerecompany.com>
2013-05-03 14:34:21 +0900

    add compile time assertion for pad. re-arrange control variables.

--- include/message_queue.h
+++ include/message_queue.h
@@ -17,6 +17,16 @@
 #define MQ_CAS_LOCK(x) do {} while (CAS(&(x), 0, 1) == 1)
 #define MQ_CAS_UNLOCK(x) do {CAS(&(x), 1, 0);} while (0)
 
+template<bool> struct CompileTimeAssertion;
+template<> struct CompileTimeAssertion<true> {};
+
+#define STATIC_ASSERT(expr, msg)\
+{\
+    ::CompileTimeAssertion<((expr) != 0)> ASSERTION_##msg;\
+    (void)ASSERTION_##msg;\
+}
+
+
 // Assumption:
 //  - 32bit pointer and 32bit integer operation is atomic.
 //  - cache line size is 64 bytes.
@@ -61,6 +71,14 @@ public:
         receive_wait_(false),
         n_receiver_(0)
     {
+        STATIC_ASSERT(((sizeof(T*) + sizeof(bool) + sizeof(Node*)) <
+                      MQ_CACHE_LINE_SIZE),
+                      Size_Of_Node_Data_Should_Be_Smaller_Than_CacheLine_Size);
+        STATIC_ASSERT((sizeof(pthread_mutex_t) < MQ_CACHE_LINE_SIZE),
+                      Size_Of_Mutex_Should_Be_Smaller_Than_CacheLine_Size);
+        STATIC_ASSERT((sizeof(pthread_cond_t) < MQ_CACHE_LINE_SIZE),
+                      Size_Of_Cond_Should_Be_Smaller_Than_CacheLine_Size);
+
         pthread_mutex_init(&receive_mutex_, NULL);
         pthread_cond_init(&receive_wait_cond_, NULL);
         pthread_mutex_init(&receive_wait_mutex_, NULL);
@@ -168,31 +186,34 @@ private:
                     sizeof(Node*)];
     };
 
-    pthread_mutex_t receive_mutex_;
-    pthread_cond_t receive_wait_cond_;
-    pthread_mutex_t receive_wait_mutex_;
-    bool receive_wait_;
-    int n_receiver_;
-    uint8_t pad0[MQ_CACHE_LINE_SIZE -
-                 sizeof(receive_mutex_) -
-                 sizeof(receive_wait_cond_) -
-                 sizeof(receive_wait_mutex_) -
-                 sizeof(receive_wait_) -
-                 sizeof(n_receiver_)
-                 ];
+    uint8_t pad0[MQ_CACHE_LINE_SIZE];
 
-    uint8_t pad1[MQ_CACHE_LINE_SIZE];
+    pthread_mutex_t receive_mutex_;
+    uint8_t pad1[MQ_CACHE_LINE_SIZE - sizeof(receive_mutex_)];
+
+    pthread_cond_t receive_wait_cond_;
+    uint8_t pad2[MQ_CACHE_LINE_SIZE - sizeof(receive_wait_cond_)];
+
+    pthread_mutex_t receive_wait_mutex_;
+    uint8_t pad3[MQ_CACHE_LINE_SIZE - sizeof(receive_wait_mutex_)];
+
+    bool receive_wait_;
+    uint8_t pad4[MQ_CACHE_LINE_SIZE - sizeof(receive_wait_)];
+
+    int n_receiver_;
+    uint8_t pad5[MQ_CACHE_LINE_SIZE - sizeof(n_receiver_)];
+
     Node* volatile first_;
-    uint8_t pad2[MQ_CACHE_LINE_SIZE - sizeof(first_)];
+    uint8_t pad6[MQ_CACHE_LINE_SIZE - sizeof(first_)];
 
     volatile int32_t receiveLock_;
-    uint8_t pad3[MQ_CACHE_LINE_SIZE - sizeof(receiveLock_)];
+    uint8_t pad7[MQ_CACHE_LINE_SIZE - sizeof(receiveLock_)];
 
     Node* volatile last_;
-    uint8_t pad4[MQ_CACHE_LINE_SIZE - sizeof(last_)];
+    uint8_t pad8[MQ_CACHE_LINE_SIZE - sizeof(last_)];
 
     volatile int32_t sendLock_;
-    uint8_t pad5[MQ_CACHE_LINE_SIZE - sizeof(sendLock_)];
+    uint8_t pad9[MQ_CACHE_LINE_SIZE - sizeof(sendLock_)];
 };
 
 #endif // !MESSAGE_QUEUE_INCLUDED
